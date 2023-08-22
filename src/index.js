b9ff49190b976a211964f37b88ea8dc354ca052b
jdeking14 <javicfgs2015@gmail.com>
2023-08-22 21:58:59 +0200

    Enrutador, handler y data del request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const http = require('node:http');
+const http = require('http');
 const url = require('url');
-
-const server = http.createServer((req, res) => {
+const StringDecoder = require('string_decoder').StringDecoder;
+const callbackDelServidor = http.createServer((req, res) => {
     //1. obtener url desde el objeto request
     const urlActual = req.url
     const urlparseada = url.parse(urlActual,true);//el true hace que se convierta en querystain y se pase a un objeto
@@ -16,18 +16,69 @@ const server = http.createServer((req, res) => {
     //3.3 obtener headers
     const {headers = {}} = req;
     console.log(headers);
-    // 4. enviar respuesta dependiendo de la ruta
-    switch (rutaLimpia) {
-      case 'ruta':
-        res.end('estas en /ruta');
-        break;
-        default:
-          res.end('desconocida');
+    //3.4 obtener el payload, en el caso de haber uno
+    const decoder = new StringDecoder('utf-8'); // explicacion esto lo que va a hacer es obtener un string, una fuente de datos que en este caso la manda un navegador en pedazos
+    
+    //3.4.1 ir acumulando la data cuando el request reciba un payload
+    const buffer = [];
+    req.on('data', (chunk) => {
+        buffer.push(chunk);
+    });
+    req.on('end', () => {
+        const payload = Buffer.concat(buffer).toString('utf-8');
+    });
+    
+
+    //3.4.2 Terminar de acumular datos y decirle al decoder que finalize
+    req.on('end', () => {
+      buffer += decoder.end();
+
+    //3.5 ordenar los datos de respuesta
+    const data = {
+      ruta: rutaLimpia,
+      metodo: metodo,
+      query: query,
+      headers: headers,
+      payload: payload 
+    };
+
+    //3.6 elegir el manejador dependiendo de la ruta y asignarle funcion que el enrutador tiene
+
+
+    let handler;
+    if(rutaLimpia){
+      handler = enrutador[rutaLimpia] || enrutador.default;
+    }else{
+      handler = enrutador.default;
     }
-   
+
+      // 4. ejecutar handler (manejador) para enviar la respuesta al cliente
+      if(typeof handler === 'function'){
+        handler(data, (statusCode = 200, mensaje) => {
+          const respuesta = JSON.stringify(mensaje);
+          res.writeHead(statusCode);
+          //linea donde realmente ya estamos respondiendo a la aplicacion cliente
+          res.end(respuesta);
+        });
+      }
+      
+    });
     //console.log({ urlActual: urlparseada})
     //no se puede poner mas res.end
 });
+
+const enrutador = {
+  ruta: (data, callback) => {
+    callback(200, {mensaje: 'Esta ruta es /ruta'});
+  },
+  default: (data, callback) => {
+    callback(404, {mensaje: 'Esta ruta no existe'});
+  }
+};
+
+
+const server = http.createServer(callbackDelServidor);
+
 
 server.listen(8000, ()=> {
     console.log('servidor escuchando en el puerto 8000');
